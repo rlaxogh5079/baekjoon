9df22e6f512161edc08fb9a44762e53e55dba0d7
rlaxogh5079 <rlaxogh507906@gmail.com>
2024-09-18 22:13:11 +0900

    01. 입출력과 사칙연산

---
+++ 01. 입출력과 사칙연산/05.py
@@ -0,0 +1,4 @@
+import sys
+
+A, B = map(int, sys.stdin.readline().split())
+print("%.9f" % (A / B))
---
+++ 01. 입출력과 사칙연산/10.py
@@ -0,0 +1,9 @@
+import sys
+
+A = int(sys.stdin.readline())
+B = int(sys.stdin.readline())
+
+print(A * (B % 10))
+print(A * ((B // 10) % 10))
+print(A * (B // 100))
+print(A * B)
